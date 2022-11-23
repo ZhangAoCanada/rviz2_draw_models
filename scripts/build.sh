deef0e9868a17daaad6f279112955cabd4da7b89
张澳 <h-zhangao@voyah.com.cn>
2022-11-23 08:47:03 +0800

    initialization

---
+++ scripts/build.sh
@@ -0,0 +1,3 @@
+cd ~/dev_ws
+colcon build --packages-select mesh_publisher
+cd ~/dev_ws/src/mesh_publisher/scripts
---
+++ scripts/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ~/dev_ws/install/local_setup.sh 
+ros2 run mesh_publisher mesh_test
