d4330f4742d7c6b18cd63f510990086a53e108be
张澳 <h-zhangao@voyah.com.cn>
2022-11-23 16:32:16 +0800

    update a lil

--- src/draw.cpp
+++ src/draw.cpp
@@ -10,7 +10,7 @@
 
 #include <tf2/LinearMath/Quaternion.h>
 
-#include <jsoncpp/json/json.h>
+// #include <jsoncpp/json/json.h>
 
 class RvizDrawCube : public rclcpp::Node
 {
@@ -32,27 +32,27 @@ private:
         visualization_msgs::msg::MarkerArray markerArray;
 
         visualization_msgs::msg::Marker marker1;
-        makeMeshMarker(marker1, 0, 0, 0, 0, 0.005, 0.0, 
+        makeMeshMarker(marker1, 0, 0, 0, 0, 0.005, 0.0, M_PI, 
             "file:///home/za/dev_ws/src/mesh_publisher/models/car/Hybrid.obj");
         markerArray.markers.push_back(marker1);
 
         visualization_msgs::msg::Marker marker2;
-        makeMeshMarker(marker2, 1, 5, 0, 0, 0.5, 0.0, 
+        makeMeshMarker(marker2, 1, 5, 0, 0, 0.5, 0.0, 0.0,  
             "file:///home/za/dev_ws/src/mesh_publisher/models/truck/pickup.dae");
         markerArray.markers.push_back(marker2);
 
         visualization_msgs::msg::Marker marker3;
-        makeMeshMarker(marker3, 2, -5, 0, 0, 0.005, 0.0,  
+        makeMeshMarker(marker3, 2, -5, 0, 0, 0.005, 0.0, M_PI, 
             "file:///home/za/dev_ws/src/mesh_publisher/models/bus/bus.obj");
         markerArray.markers.push_back(marker3);
 
         visualization_msgs::msg::Marker marker4;
-        makeMeshMarker(marker4, 3, 10, 0, 0, 0.8, M_PI/2,  
+        makeMeshMarker(marker4, 3, 10, 0, 0, 0.8, M_PI/2, M_PI, 
             "file:///home/za/dev_ws/src/mesh_publisher/models/bike/bicycle.obj");
         markerArray.markers.push_back(marker4);
 
         visualization_msgs::msg::Marker marker5;
-        makeMeshMarker(marker5, 4, -10, 0, 0, 0.5, 0.0, 
+        makeMeshMarker(marker5, 4, -10, 0, 0, 0.5, 0.0, M_PI,
             "file:///home/za/dev_ws/src/mesh_publisher/models/people/casual_female.dae");
         markerArray.markers.push_back(marker5);
        
@@ -60,7 +60,7 @@ private:
         RCLCPP_INFO(this->get_logger(), "Publishing num of markers: %d", static_cast<int>(markerArray.markers.size()));
     }
 
-    void makeMeshMarker(visualization_msgs::msg::Marker &marker, const int id,  const float x, const float y, const float z, const float scale, const float angleOffset, const std::string& mesh_path)
+    void makeMeshMarker(visualization_msgs::msg::Marker &marker, const int id,  const float x, const float y, const float z, const float scale, const float angleOffset, const float yawOffset, const std::string& mesh_path)
     {
         marker.header.frame_id = "map";
         marker.header.stamp = this->now();
@@ -77,7 +77,7 @@ private:
         // float vy = static_cast<float>(object_msg.lat_vel) / 1000.0f;
         // float angle = atan2(vy, vx);
         float angle = static_cast<float>(0) / 1000.0f * M_PI / 180.0f;
-        orientation.setRPY(angleOffset, 0.0, angle);
+        orientation.setRPY(angleOffset, 0.0, angle + yawOffset);
         marker.pose.orientation.x = orientation.x();
         marker.pose.orientation.y = orientation.y();
         marker.pose.orientation.z = orientation.z();
@@ -97,16 +97,16 @@ private:
         marker.lifetime = rclcpp::Duration::from_seconds(0.1); 
     }
 
-    void readDetections(const std::string& json_path)
-    {
-        Json::Reader reader;
-        std::ifstream ifs(json_path);
-        if (!reader.parse(ifs, json_val_, false))
-        {
-            RCLCPP_ERROR(this->get_logger(), "Failed to parse json file: %s", json_path.c_str());
-            return;
-        }
-    }
+    // void readDetections(const std::string& json_path)
+    // {
+    //     Json::Reader reader;
+    //     std::ifstream ifs(json_path);
+    //     if (!reader.parse(ifs, json_val_, false))
+    //     {
+    //         RCLCPP_ERROR(this->get_logger(), "Failed to parse json file: %s", json_path.c_str());
+    //         return;
+    //     }
+    // }
 
     void chooseClassModel(const std::string& class_name)
     {
@@ -143,7 +143,7 @@ private:
     visualization_msgs::msg::MarkerArray sfusion_markerArray_;
 
     std::string model_path_;
-    Json::Value json_val_;
+    // Json::Value json_val_;
 };
 
 
